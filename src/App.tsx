b6f30bdd8975bb9f23173448f33a5513338b4be4
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-07 22:34:59 -0500

    built: create repository and add template in jsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Word Search game</h1>
+      </header>
+      <main>
+        <section>
+          <span>time</span>
+          <button>clear</button>
+          <button>reset</button>
+        </section>
+        <section>
+          <h2>You have 3 words of 10 left to win</h2>
+          <ul>
+            <li>sssss</li>
+          </ul>
+          <ul>
+            <li>aaaa</li>
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
