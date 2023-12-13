5244fb94cd3afd0f3c3d39c579c9726597106cdf
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-12 22:40:08 -0500

    wip: add logic for word list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { WORD_LIST } from "./constants";
+
+const initialWordList = (object: any): string[] => {
+  const keys = Object.keys(object);
+  const random = keys[Math.floor(Math.random() * keys.length)];
+  const listRandom = object[`${random}`];
+  return listRandom;
+};
+initialWordList(WORD_LIST);
+
 function App() {
+  const [wordList, setWordList] = useState(initialWordList(WORD_LIST));
+  const [checkWord, setCheckWord] = useState<string[]>([]);
+
   return (
     <>
-      <header>
+      <header
+        style={{
+          width: "100%",
+          textAlign: "center",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
         <h1>Word Search game</h1>
       </header>
       <main>
@@ -11,15 +32,26 @@ function App() {
           <button>reset</button>
         </section>
         <section>
-          <h2>You have 3 words of 10 left to win</h2>
+          <h2>
+            You have {checkWord.length} words of {wordList.length} left to win
+          </h2>
           <ul>
-            <li>sssss</li>
+            {wordList.map((cur, idx) => (
+              <li key={idx} onClick={() => console.log("checked")}>
+                {cur}
+              </li>
+            ))}
           </ul>
           <ul>
-            <li>aaaa</li>
+            {checkWord.map((cur, idx) => (
+              <li key={idx} onClick={() => console.log("not checked")}>
+                {cur}
+              </li>
+            ))}
           </ul>
         </section>
       </main>
+      <footer>by Lachicagadiadora</footer>
     </>
   );
 }
